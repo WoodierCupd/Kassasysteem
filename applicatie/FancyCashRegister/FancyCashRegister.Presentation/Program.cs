574b1dcf4d2ba0227646adf5c1e39efda91e198e
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-06-10 12:02:25 +0200

    test for serilog (has a error)

--- applicatie/FancyCashRegister/FancyCashRegister.Presentation/Program.cs
+++ applicatie/FancyCashRegister/FancyCashRegister.Presentation/Program.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Serilog;
+using System.IO;
+using Serilog.Debugging;
 
 namespace FancyCashRegisterForms
 {
@@ -13,8 +16,34 @@ namespace FancyCashRegisterForms
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            /*
+            SelfLog.Enable(Console.Out);
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
+
+            for (var i = 0; i < 1000000; ++i)
+            {
+                Log.Information("Hello, file logger!");
+            }
+
+            Log.CloseAndFlush();
+
+            sw.Stop();
+
+            Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Size: {new FileInfo("log.txt").Length}");
+
+            Console.WriteLine("Press any key to delete the temporary log file...");
+            Console.ReadKey(true);
+
+            File.Delete("log.txt");
+            */
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
